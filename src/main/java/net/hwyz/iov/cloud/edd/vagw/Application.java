52299849c4fc75c9a98333a3b798aef0721bce80
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-06-26 23:33:46 +0800

    feat: add AppConfig with ObjectMapper; update Application entry point

--- src/main/java/net/hwyz/iov/cloud/edd/vagw/Application.java
+++ src/main/java/net/hwyz/iov/cloud/edd/vagw/Application.java
@@ -1,25 +1,21 @@
 package net.hwyz.iov.cloud.edd.vagw;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * 启动类
- *
- * @author hwyz_leo
- */
 @Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        System.setProperty("nacos.logging.default.config.enabled","false");
-        SpringApplication.run(Application.class, args);
-        logger.info("应用启动完成");
+    static {
+        System.setProperty("nacos.logging.default.config.enabled", "false");
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("VAGW started on port {}", System.getProperty("server.port", "10804"));
+    }
 }
